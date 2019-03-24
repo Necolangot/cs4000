0f2780e5b0b5003ce8d322867b9ed54ff3e7bcdc
necolangot <dl749615@ohio.edu>
2019-03-23 23:30:30 -0400

    updated battle_client.cc

--- hw4/battle_client.cc
+++ hw4/battle_client.cc
@@ -1,5 +1,51 @@
 using namespace std;
 
+public enum state{
+	vertical,
+	dright,
+	horizontal,
+	dleft,
+}
+struct boardstate{
+	void left_state(int cur_row, int cur_col, vector<vector<int> >& board);
+	void right_state(int cur_row, int cur_col, vector<vector<int> >& board);
+	state cstate;
+};
+
+void left_state(int cur_row, int cur_col, vector<vector<int> >& board){
+	switch(cstate){
+		case vertical:
+			if ((cur_row - 1) < 0 && (cur_col - 1) < 0){
+				board[3][3] = 0;
+				board[cur_row+1][cur_col+1] = 0;
+			}
+			if ((cur_row-1) < 0 && (cur_col)
+			break;
+		case dright:
+			if ((cur_row - 1) < 0){
+				board[3][cur_col] = 0;
+				board[cur_row+1][cur_col] = 0;
+			}
+			else if ((cur_row + 1) > 3){
+				board[0][cur_col] = 0;
+				board[cur_row-1][cur_col] = 0;
+			}
+			else{ 
+				board[cur_row+1][cur_col] = 0;
+				board[cur_row-1][cur_col] = 0;
+			}
+			cstate = vertical;
+			break;
+		case horizontal:
+
+			break;
+		case dleft:
+
+			break;
+	}
+}
+
+
 void draw_top_matrix(vector<vector<int> > &board,
 		     int cur_row,
 		     int cur_col) {
@@ -18,13 +64,13 @@ void draw_top_matrix(vector<vector<int> > &board,
       if (board[i][j] == 1) {
 	printw(" ");
       } 
-	  else if (board[i][j] == 1) {
+	  else if (board[i][j] == 0) {
 		printw("X");
       }
 	  else if (board[i][j] == 2) {
 		printw("H");
       }
-	  else if (board[i][j] == 1) {
+	  else if (board[i][j] == 3) {
 		printw("M");
       }
 	  
@@ -63,14 +109,7 @@ int main(int argc,char *argv[1]) {
   int cur_col=0;
   int ch;
 	
-  vector<vector<int> > board;
-  for (int i=0;i<4;i++) {
-    vector<int> t;
-    for (int j=0;j<4;j++) {
-      t.push_back(1);
-    }
-    board.push_back(t);
-  }
+  board board1;  
   
   // Screen initialization
 
@@ -86,66 +125,103 @@ int main(int argc,char *argv[1]) {
   cbreak();  // Pass all characters to this program!
 
   keypad(stdscr, TRUE); // Grab the special keys, arrow keys, etc.
-
+  vector<vector<int> > board;
+  	for (int i=0;i<4;i++) {
+    vector<int> t;
+    for (int j=0;j<4;j++) {
+      t.push_back(1);
+    }
+    board.push_back(t);
+  }
   // Paint the row and column markers.
   //paint_markers(rows,cols,10,0,0);
   // Redraw the screen.
   refresh();
 	
 
-  draw_top_matrix(board,0,0);
+  draw_top_matrix(board1.getb(),0,0);
   
 
   // I/O Loop....
   // Stop when the q Key is hit.
   //
-  for (int i = 0;i < 3;i++){
-	  while ((ch = getch())!='q') {
-		switch (ch) {
-		case ' ':  
-		board[cur_row][cur_col]=0;
-		  draw_top_matrix(board,cur_row,cur_col);
-		  // Redraw the screen.
-		  refresh();
-
-		  break;
-		case KEY_RIGHT:
-		  cur_col++;
-		  cur_col%=4;
-		  draw_top_matrix(board,cur_row,cur_col);
-		  // Redraw the screen.
-		  refresh();
-		  break;
-		case KEY_LEFT:
-		  cur_col--;
-		  cur_col = (4+cur_col)%4;
-		  draw_top_matrix(board,cur_row,cur_col);
-		  // Redraw the screen.
-		  refresh();
-		  break;
-		case KEY_UP:
-		  cur_row--;
-		  cur_row=(4+cur_row) % 4;
-		  draw_top_matrix(board,cur_row,cur_col);
-		  
-		  //      paint_markers(rows,cols,10,cur_row,cur_col);
-		  // Redraw the screen.
-		  refresh();
-		  break;
-		case KEY_DOWN:
-		  cur_row++;
-		  cur_row%=4;
-		  draw_top_matrix(board,cur_row,cur_col);
-				//paint_markers(rows,cols,10,cur_row,cur_col);
-		  // Redraw the screen.
-		  refresh();
-		  break;
+  boardstate bs;
+  bool placed = false;
+  bool finished = false;
+	while ((ch = getch())!='q' && !finished) {
+		if placed {
+			switch(ch){
+				case KEY_LEFT:
+					bs.left_state(cur_row, cur_col, board);
+					break;
+				case KEY_RIGHT:
+					bs.right_state(cur_row, cur_col, board);
+					break;
+				case ' ':
+					finished = true;
+					break;
+				case 'q':
+				placed = false;
+				break;
+			}
+		}
+		else{
+			switch (ch) {
+			case ' ':  
+				board[cur_row][cur_col]=0;
+				bs.cstate = vertical;
+				if ((cur_row - 1) < 0){
+					board[3][cur_col] = 0;
+					board[cur_row+1][cur_col] = 0;
+				}
+				else if ((cur_row + 1) > 3){
+					board[0][cur_col] = 0;
+					board[cur_row-1][cur_col] = 0;
+				}
+				else{ 
+					board[cur_row+1][cur_col] = 0;
+					board[cur_row-1][cur_col] = 0;
+				}
+			  	draw_top_matrix(board,cur_row,cur_col);
+			  	// Redraw the screen.
+			  	refresh();
+			  	placed = true;
+			  	break;
+			case KEY_RIGHT:
+			  cur_col++;
+			  cur_col%=4;
+			  draw_top_matrix(board,cur_row,cur_col);
+			  // Redraw the screen.
+			  refresh();
+			  break;
+			case KEY_LEFT:
+			  cur_col--;
+			  cur_col = (4+cur_col)%4;
+			  draw_top_matrix(board,cur_row,cur_col);
+			  // Redraw the screen.
+			  refresh();
+			  break;
+			case KEY_UP:
+			  cur_row--;
+			  cur_row=(4+cur_row) % 4;
+			  draw_top_matrix(board,cur_row,cur_col);
+			  
+			  //      paint_markers(rows,cols,10,cur_row,cur_col);
+			  // Redraw the screen.
+			  refresh();
+			  break;
+			case KEY_DOWN:
+			  cur_row++;
+			  cur_row%=4;
+			  draw_top_matrix(board,cur_row,cur_col);
+					//paint_markers(rows,cols,10,cur_row,cur_col);
+			  // Redraw the screen.
+			  refresh();
+			  break;
 
 
 		}
-	  }
-
-	}
+ }
   
 
   
